000b22e51d2e7f02ee7c2d825ec8903259cb0858
furk4nk <95353888+furk4nk@users.noreply.github.com>
2023-07-07 22:48:38 +0300

    Add project files.

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230707194005_mig_RelationShip_AppUser.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class mig_RelationShip_AppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerAccountBalance",
+                table: "CustomerAccounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserID",
+                table: "CustomerAccounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccounts_AppUserID",
+                table: "CustomerAccounts",
+                column: "AppUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccounts_AspNetUsers_AppUserID",
+                table: "CustomerAccounts",
+                column: "AppUserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccounts_AspNetUsers_AppUserID",
+                table: "CustomerAccounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccounts_AppUserID",
+                table: "CustomerAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserID",
+                table: "CustomerAccounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerAccountBalance",
+                table: "CustomerAccounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,17 @@
+﻿using EasyCashIdentityProject.EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess : BaseEntity
+    {
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+    }
+}
+    
