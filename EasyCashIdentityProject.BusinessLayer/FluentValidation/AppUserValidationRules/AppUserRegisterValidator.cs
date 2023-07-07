bc788f7f61f896a75e47c4d30783e09bb535d3bd
furk4nk <95353888+furk4nk@users.noreply.github.com>
2023-07-08 01:15:47 +0300

    DtoLayerAdded

---
+++ EasyCashIdentityProject.BusinessLayer/FluentValidation/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,41 @@
+﻿using EasyCashIdentityProject.DtoLayer.DTOs.AppUserDtos;
+using FluentValidation;
+
+namespace EasyCashIdentityProject.BusinessLayer.FluentValidation.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            //Name Rules
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim Alanı Boş Geçilemez");
+
+
+
+            //Surname Rules
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyisim Alanı Boş Geçilemez");
+
+
+
+            //UserName Rules
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı Adı Boş Geçilemez");
+
+
+
+            //Email Rules
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen Geçerli Bir Email Adresi Giriniz");
+
+
+
+            //Password Rules
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez");
+
+
+
+            //Confirm Password Rules
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilemez")
+                .Equal(y => y.Password).WithMessage("Şifreler Uyuşmuyor");
+
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DtoLayer/DTOs/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using EasyCashIdentityProject.EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.DTOs.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
+        
